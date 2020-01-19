be46b24620d1d674e63e3f415e953b288d000bfc
Fabio Brito <fabio.britto15@gmail.com>
2020-01-19 16:53:57 -0300

    config cors and add bcryptjs package

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,5 @@
 import { EntityRepository, Repository } from "typeorm";
-import * as bcrypt from "bcrypt";
+import * as bcrypt from "bcryptjs";
 import { User } from "./user.entity";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import {
--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,13 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
+  if (process.env.NODE_ENV === "development") {
+    app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin });
+    logger.log(`Accepting requests from origin "${serverConfig.origin}"`);
+  }
+
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log("App is running");
